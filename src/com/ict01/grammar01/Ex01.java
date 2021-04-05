3b9bb085a44f32b7ba791a9f99ae4e3bcbc2cfb1
wish813 <wish813@naver.com>
2021-04-05 15:05:02 +0900

    ㅇㅇㅇㅇ

--- src/com/ict01/grammar01/Ex01.java
+++ src/com/ict01/grammar01/Ex01.java
@@ -2,12 +2,15 @@ package com.ict01.grammar01;
 class Ex01{
             public static void main(String[] args){
                     System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+                    System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+                    System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+                    System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+                    System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+                    System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+                    System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+                    System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+                    System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
+                    System.out.println("ÀÚ¹ÙÀÇ ²Ş!");
             }
 }
-
-
-
-
-
-
 
--- src/com/ict04/exception/Ex01.java
+++ src/com/ict04/exception/Ex01.java
@@ -1,5 +1,10 @@
 package com.ict04.exception;
 
 public class Ex01 {
-
+	public static void main(String[] args) {
+		// 자바에서 오류가 발생 하면 무조건 프로그램을 종료 시킨다.
+		// 그러나 가벼운 예외나 예상된 예외를 별도로 처리하는 과정을 방법을 사용하면
+		// 무조건 프로그램이 종료되는 것이 아니라 정상 종료를 할 수 있도록 하는 방법을
+		// 예외 처리라고 한다.
+	}
 }
